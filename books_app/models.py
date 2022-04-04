444272b71928368132d858a99d5f877c349f6a1e
Jane <jane.anqi.cui@gmail.com>
2022-04-04 12:10:50 -0400

    added models and html

--- books_app/models.py
+++ books_app/models.py
@@ -9,6 +9,11 @@ class Audience(enum.Enum):
     ADULT = 3
     ALL = 4
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    favorite_books = db.relationship('Book', secondary='favorite_book', lazy='subquery', backref=db.backref('users', lazy=True))
+
 class Book(db.Model):
     """Book model."""
     id = db.Column(db.Integer, primary_key=True)
@@ -24,12 +29,11 @@ class Book(db.Model):
 
     # The genres, e.g. fiction, sci-fi, fantasy
     genres = db.relationship('Genre', secondary='book_genre', back_populates='books')
-
     def __str__(self):
         return f'<Book: {self.title}>'
 
     def __repr__(self):
-        return f'<Book: {self.title}>'
+        return f'<Book: {self.title} {self.publish_date} {self.audience}>'
 
 class Author(db.Model):
     """Author model."""
@@ -58,4 +62,9 @@ class Genre(db.Model):
 book_genre_table = db.Table('book_genre',
     db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
     db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+)
+
+favorite_book_table = db.Table('favorite_book', 
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True), 
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
 )
